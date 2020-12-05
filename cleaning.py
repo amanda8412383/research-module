d28eeebad046cbb0d32b5cc739bab0882666b675
amanda8412383 <amanda8412383yahoo@gmail.com>
2020-12-05 01:07:23 +0100

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -7,7 +7,7 @@
 # - Bosnia Herzegovina have no record in humanitarian aids data.
 # - 54 countries do not have ODA record
 # - 63 countries do not have aid received record
-# - government expense is as GDP % but in current and constant value wasn't specified and the data looks fishy  
+#  
 # 
 # ### data output 
 # | Variable     |      Content                                                   |
@@ -21,11 +21,13 @@
 # |pledge2003 ~ pledge2019|pledging humanitarian aid in US dollar by year        |
 # |funding2003_gdp ~ funding2019_gdp| funding / gdp the year * 100       |
 # |gdpcapita2003 ~ gdpcapita2019| gdp per capita in current US dollar by year  |
-# |govexpense2003 ~ govexpense2019| General government total expenditure as % of GDP  |
 # |region| region group from WDI data|
 # |income_type| income group from WDI data|
+# |aid_boolean| True if the country ever receive net official aid between 2003 ~ 2019|
 # |pop2003 ~ pop2019| population data by year |
 # |oda2003 ~ oda2019|Net Official development assistance in current US dollar |
+# |govexpense2003 ~ govexpense2019| General government total expenditure as % of GDP  |
+# 
 # 
 # 
 # 
@@ -36,7 +38,15 @@
 import pandas as pd
 import glob
 from auxiliary import *
+import zipfile
+
 pd.set_option('display.max_columns', 500)
+
+
+# %%
+
+with zipfile.ZipFile('data.zip', 'r') as zip_ref:
+    zip_ref.extractall('')
 
 # %% [markdown]
 # ### read in  democracy index from The Economist's Democracy Index
@@ -120,10 +130,7 @@ gps_demo_donate[gps_demo_donate.filter(regex='(funding|pledge)20*', axis=1).isna
 # ### read in GDP in current US dollar data from WDI and merge with previous data frame
 
 # %%
-try:
-	gdp = pd.read_excel('data/GDP_by_country_by_year.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
-except:
-	gdp = pd.read_excel('data/GDP_by_country_by_year.xlsx', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
+gdp = pd.read_excel('data/GDP_by_country_by_year.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
 gdp = gdp.iloc[:,-18 : -1]
 gdp = gdp.add_prefix('gdp')
 gdp.reset_index()
@@ -143,22 +150,19 @@ gps_demo_donate_gdp.head()
 # ### adding columns of funding / GDP * 100 by year
 
 # %%
-df_funding = convert_percent('funding','gdp', gps_demo_donate_gdp)
+df_funding = convert_percent('funding','gdp', gps_demo_donate_gdp, ratio=100)
 df_funding.head()
 
 # %% [markdown]
-# ### adding General government total expenditure as % of GDP & GDP per capita in current US dollar from IMF 
+# ### adding GDP per capita in current US dollar from WDI
+# 
+# <!---adding General government total expenditure as % of GDP from IMF --->
 
 # %%
-imf = pd.read_excel('data/IMF_Macro_Data_2020.xlsx').rename(columns={"ISO": "isocode"}).set_index('isocode')
-imf_rename = imf.replace({'Gross domestic product per capita, current prices': 'gdpcapita', 'General government total expenditure': 'govexpense'})
-imf_capita = imf_rename.query('`Subject Descriptor` == "gdpcapita"')
-capita_filter = imf_capita.filter(regex='^20(10|11|12|.*[3456789]$)', axis=1).add_prefix('gdpcapita')
+capita = pd.read_excel('data/gdppercapita.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
+capita_filter = capita.filter(regex='^20(10|11|12|.*[3456789]$)', axis=1).add_prefix('gdpcapita')
 
-imf_expense = imf_rename.query('`Subject Descriptor` == "govexpense"')
-expense_filter = imf_expense.filter(regex='^20(10|11|12|.*[3456789]$)', axis=1).add_prefix('govexpense')
-
-expense_filter.head()
+capita_filter.head()
 
 
 # %%
@@ -166,25 +170,30 @@ df_add_capita = pd.merge(df_funding, capita_filter, how='left', on="isocode")
 # df_add_capita.shape
 # df_add_capita[df_add_capita.filter(regex='gdpcapita.*', axis=1).isna().all(axis=1)]
 
-df_add_expense = pd.merge(df_add_capita, expense_filter, how='left', on="isocode")
-# df_add_expense.shape
-# df_add_expense[df_add_expense.filter(regex='govexpense.*', axis=1).isna().all(axis=1)]
+
+
+# %% [markdown]
+# ### adding government expense as % of GDP from imf
+# 
+
+# %%
+govexpense = pd.read_csv('data/govexpense.csv').set_index('isocode')
+df_add_govexpense = pd.merge(df_add_capita, govexpense, how='left', on="isocode")
+df_add_govexpense.shape
+# df_add_govexpense[df_add_govexpense.filter(regex='govexpense.*', axis=1).isna().all(axis=1)]
 
 # %% [markdown]
 # ### adding region and income group data from WDI
 
 # %%
-try:
-	region = pd.read_csv('data/WDICountry.csv').rename(columns={"Country Code": "isocode"}).set_index('isocode')
-except:
-	region = pd.read_excel('data/WDICountry.csv.xlsx').rename(columns={"Country Code": "isocode"}).set_index('isocode')
+region = pd.read_csv('data/WDICountry.csv').rename(columns={"Country Code": "isocode"}).set_index('isocode')
 region_rename = region.rename({'Region': 'region', 'Income Group': 'income_type'}, axis=1)
 region_filter = region_rename.filter(items=['region', 'income_type'], axis=1)
 region_filter.head()
 
 
 # %%
-df_add_region = pd.merge(df_add_expense, region_filter, how='left', on="isocode")
+df_add_region = pd.merge(df_add_govexpense, region_filter, how='left', on="isocode")
 # df_add_region.shape
 # df_add_region[df_add_region.filter(items=['region', 'income_type'], axis=1).isna().any(axis=1)]
 
@@ -207,10 +216,7 @@ df_add_pop = pd.merge(df_add_region, pop_filter, how='left', on="isocode")
 # ### notice 54/76 countries have no ODA record from 2003 ~ 2019
 
 # %%
-try:
-	oda = pd.read_excel('data/oda.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
-except:
-	oda = pd.read_excel('data/oda.xlsx', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
+oda = pd.read_excel('data/oda.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
 oda_filter = oda.filter(regex='^20(10|11|12|.*[3456789]$)', axis=1).add_prefix('oda')
 oda_filter.head()
 
@@ -231,22 +237,31 @@ df_add_oda[df_add_oda.filter(regex='oda.*', axis=1).isna().all(axis=1)].shape
 # df_add_odagni[df_add_odagni.filter(regex='odagni.*', axis=1).isna().all(axis=1)].shape
 
 # %% [markdown]
-# ### adding net official aid recieve in current US dollar 
+# ### adding net official aid recieve or not in current US dollar 
+# ### our countries of interest only appear in 2003, 2004, 2008, 2009, 2011
 # ### notice 63/76 countries does not have any record
 
 # %%
-try:
-	aid = pd.read_excel('data/offial aid received.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
-except:
-	aid = pd.read_excel('data/offial aid received.xlsx', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
+
+aid = pd.read_excel('data/offial aid received.xls', sheet_name='Data', skiprows=3).rename(columns={"Country Code": "isocode"}).set_index('isocode')
 aid_filter = aid.filter(regex='^20(10|11|12|.*[3456789]$)', axis=1).add_prefix('aid')
 aid_filter.head()
 
 
 # %%
-df_add_aid = pd.merge(df_add_oda, aid_filter, how='left', on="isocode")
-# df_add_aid.shape
-df_add_aid[df_add_aid.filter(regex='aid.*', axis=1).isna().all(axis=1)].shape
+aid_boolean = aid_filter.mean(axis=1).isna()
+df_add_aid = pd.merge(df_add_oda, aid_boolean.rename('aid_boolean'), how='left', on="isocode")
+df_add_aid.shape
+
+
+# %%
+### adding columns of GDP / population * 100 by year and compare with GDP per capita to ensure corectness
+# df_capita = convert_percent('gdp','pop', df_add_aid)
+# df_capita[['gdp2003_pop', 'gdpcapita2003', 'gdp2004_pop', 'gdpcapita2004']]
+
+
+# %%
+df_add_aid.head()
 
 # %% [markdown]
 # ### output
